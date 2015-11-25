46e5a7d5865adf64c2f4adeedae2f515fb019f63
adnan <ad.sahibzada@gmail.com>
2015-11-25 16:45:13 +0500

    Remove StopWords Fucntionality Added

--- src/WordCrowd.js
+++ src/WordCrowd.js
@@ -5,9 +5,480 @@ File : WordCrowd
 This file is part of WordCrowd.
 
 */
- 
- 
- 
+
+/*********************Stop words*************************************/
+String.prototype.removeStopWords = function() {
+	var x;
+	var y;
+	var word;
+	var stop_word;
+	var regex_str;
+	var regex;
+	var cleansed_string = this.valueOf();
+	
+	var stop_words = new Array(
+		
+		'a',
+		'about',
+		'above',
+		'across',
+		'after',
+		'again',
+		'against',
+		'all',
+		'almost',
+		'alone',
+		'along',
+		'already',
+		'also',
+		'although',
+		'always',
+		'among',
+		'an',
+		'and',
+		'another',
+		'any',
+		'anybody',
+		'anyone',
+		'anything',
+		'anywhere',
+		'are',
+		'area',
+		'areas',
+		'around',
+		'as',
+		'ask',
+		'asked',
+		'asking',
+		'asks',
+		'at',
+		'away',
+		'b',
+		'back',
+		'backed',
+		'backing',
+		'backs',
+		'be',
+		'became',
+		'because',
+		'become',
+		'becomes',
+		'been',
+		'before',
+		'began',
+		'behind',
+		'being',
+		'beings',
+		'best',
+		'better',
+		'between',
+		'big',
+		'both',
+		'but',
+		'by',
+		'c',
+		'came',
+		'can',
+		'cannot',
+		'case',
+		'cases',
+		'certain',
+		'certainly',
+		'clear',
+		'clearly',
+		'come',
+		'could',
+		'd',
+		'did',
+		'differ',
+		'different',
+		'differently',
+		'do',
+		'does',
+		'done',
+		'down',
+		'down',
+		'downed',
+		'downing',
+		'downs',
+		'during',
+		'e',
+		'each',
+		'early',
+		'either',
+		'end',
+		'ended',
+		'ending',
+		'ends',
+		'enough',
+		'even',
+		'evenly',
+		'ever',
+		'every',
+		'everybody',
+		'everyone',
+		'everything',
+		'everywhere',
+		'f',
+		'face',
+		'faces',
+		'fact',
+		'facts',
+		'far',
+		'felt',
+		'few',
+		'find',
+		'finds',
+		'first',
+		'for',
+		'four',
+		'from',
+		'full',
+		'fully',
+		'further',
+		'furthered',
+		'furthering',
+		'furthers',
+		'g',
+		'gave',
+		'general',
+		'generally',
+		'get',
+		'gets',
+		'give',
+		'given',
+		'gives',
+		'go',
+		'going',
+		'good',
+		'goods',
+		'got',
+		'great',
+		'greater',
+		'greatest',
+		'group',
+		'grouped',
+		'grouping',
+		'groups',
+		'h',
+		'had',
+		'has',
+		'have',
+		'having',
+		'he',
+		'her',
+		'here',
+		'herself',
+		'high',
+		'high',
+		'high',
+		'higher',
+		'highest',
+		'him',
+		'himself',
+		'his',
+		'how',
+		'however',
+		'i',
+		'if',
+		'important',
+		'in',
+		'interest',
+		'interested',
+		'interesting',
+		'interests',
+		'into',
+		'is',
+		'it',
+		'its',
+		'itself',
+		'j',
+		'just',
+		'k',
+		'keep',
+		'keeps',
+		'kind',
+		'knew',
+		'know',
+		'known',
+		'knows',
+		'l',
+		'large',
+		'largely',
+		'last',
+		'later',
+		'latest',
+		'least',
+		'less',
+		'let',
+		'lets',
+		'like',
+		'likely',
+		'long',
+		'longer',
+		'longest',
+		'm',
+		'made',
+		'make',
+		'making',
+		'man',
+		'many',
+		'may',
+		'me',
+		'member',
+		'members',
+		'men',
+		'might',
+		'more',
+		'most',
+		'mostly',
+		'mr',
+		'mrs',
+		'much',
+		'must',
+		'my',
+		'myself',
+		'n',
+		'necessary',
+		'need',
+		'needed',
+		'needing',
+		'needs',
+		'never',
+		'new',
+		'new',
+		'newer',
+		'newest',
+		'next',
+		'no',
+		'nobody',
+		'non',
+		'noone',
+		'not',
+		'nothing',
+		'now',
+		'nowhere',
+		'number',
+		'numbers',
+		'o',
+		'of',
+		'off',
+		'often',
+		'old',
+		'older',
+		'oldest',
+		'on',
+		'once',
+		'one',
+		'only',
+		'open',
+		'opened',
+		'opening',
+		'opens',
+		'or',
+		'order',
+		'ordered',
+		'ordering',
+		'orders',
+		'other',
+		'others',
+		'our',
+		'out',
+		'over',
+		'p',
+		'part',
+		'parted',
+		'parting',
+		'parts',
+		'per',
+		'perhaps',
+		'place',
+		'places',
+		'point',
+		'pointed',
+		'pointing',
+		'points',
+		'possible',
+		'present',
+		'presented',
+		'presenting',
+		'presents',
+		'problem',
+		'problems',
+		'put',
+		'puts',
+		'q',
+		'quite',
+		'r',
+		'rather',
+		'really',
+		'right',
+		'right',
+		'room',
+		'rooms',
+		's',
+		'said',
+		'same',
+		'saw',
+		'say',
+		'says',
+		'second',
+		'seconds',
+		'see',
+		'seem',
+		'seemed',
+		'seeming',
+		'seems',
+		'sees',
+		'several',
+		'shall',
+		'she',
+		'should',
+		'show',
+		'showed',
+		'showing',
+		'shows',
+		'side',
+		'sides',
+		'since',
+		'small',
+		'smaller',
+		'smallest',
+		'so',
+		'some',
+		'somebody',
+		'someone',
+		'something',
+		'somewhere',
+		'state',
+		'states',
+		'still',
+		'still',
+		'such',
+		'sure',
+		't',
+		'take',
+		'taken',
+		'than',
+		'that',
+		'the',
+		'their',
+		'them',
+		'then',
+		'there',
+		'therefore',
+		'these',
+		'they',
+		'thing',
+		'things',
+		'think',
+		'thinks',
+		'this',
+		'those',
+		'though',
+		'thought',
+		'thoughts',
+		'three',
+		'through',
+		'thus',
+		'to',
+		'today',
+		'together',
+		'too',
+		'took',
+		'toward',
+		'turn',
+		'turned',
+		'turning',
+		'turns',
+		'two',
+		'u',
+		'under',
+		'until',
+		'up',
+		'upon',
+		'us',
+		'use',
+		'used',
+		'uses',
+		'v',
+		'very',
+		'w',
+		'want',
+		'wanted',
+		'wanting',
+		'wants',
+		'was',
+		'way',
+		'ways',
+		'we',
+		'well',
+		'wells',
+		'went',
+		'were',
+		'what',
+		'when',
+		'where',
+		'whether',
+		'which',
+		'while',
+		'who',
+		'whole',
+		'whose',
+		'why',
+		'will',
+		'with',
+		'within',
+		'without',
+		'work',
+		'worked',
+		'working',
+		'works',
+		'would',
+		'x',
+		'y',
+		'year',
+		'years',
+		'yet',
+		'you',
+		'young',
+		'younger',
+		'youngest',
+		'your',
+		'yours',
+		'z'
+		)
+
+    // Split out all the individual words in the phrase
+    words = cleansed_string.match(/[^\s]+|\s+[^\s+]$/g)
+    
+    // Review all the words
+    for(x=0; x < words.length; x++) {
+        // For each word, check all the stop words
+        for(y=0; y < stop_words.length; y++) {
+            // Get the current word
+            word = words[x].replace(/\s+|[^a-z]+/ig, "");   // Trim the word and remove non-alpha
+            
+            // Get the stop word
+            stop_word = stop_words[y];
+            
+            // If the word matches the stop word, remove it from the keywords
+            if(word.toLowerCase() == stop_word) {
+                // Build the regex
+                regex_str = "^\\s*"+stop_word+"\\s*$";      // Only word
+                regex_str += "|^\\s*"+stop_word+"\\s+";     // First word
+                regex_str += "|\\s+"+stop_word+"\\s*$";     // Last word
+                regex_str += "|\\s+"+stop_word+"\\s+";      // Word somewhere in the middle
+                regex = new RegExp(regex_str, "ig");
+                
+                // Remove the word from the keywords
+                cleansed_string = cleansed_string.replace(regex, " ");
+            }
+        }
+    }
+    return cleansed_string.replace(/^\s+|\s+$/g, "");
+}
+
 function Ziggurat(){
 
 	var jsr = 123456789;
@@ -102,22 +573,22 @@ function Ziggurat(){
 
 function wordFrequency (stringLine) {
 	
-   stringLine = stringLine.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g," ");
-   
+	stringLine = stringLine.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g," ");
+	
    //. stringLine = stringLine.removeStopWords();
    words = stringLine.split(/\s/);
    freqMap = {};
    data = [];
    words.forEach(function(w) {
-		if (!freqMap[w]) {
-			freqMap[w] = 0;
-		}
-		freqMap[w] += 1;
-	});
-	 for (var key in freqMap) {
+   	if (!freqMap[w]) {
+   		freqMap[w] = 0;
+   	}
+   	freqMap[w] += 1;
+   });
+   for (var key in freqMap) {
 		//console.log("key : "+ key + ", value :" + freqMap[key]);
 		data.push({"word":key , "size":freqMap[key]});
-	 }
+	}
 	return data;
 }
 
@@ -130,6 +601,7 @@ var WordCrowd = function (options){
 	var settings = {
 		container:'body',
 		data : 'Computer science is the scientific and practical approach to computation and its applications. It is the systematic study of the feasibility,structure, expression, and mechanization of the methodical procedures or algorithms that underlie the acquisition, representation,processing storage, communication of, and access to information, whether such information is encoded as bits.',
+		stopwordsRemove:false,
 		width:800,
 		fontsize_range : {
 			min : 14,
@@ -143,7 +615,7 @@ var WordCrowd = function (options){
 		colors:'random',
 		background :'white',
 		font_families:[ "Verdana", "Arial"]
-			
+		
 	};
 	
 	if(!options.data.trim()){
@@ -157,40 +629,46 @@ var WordCrowd = function (options){
 				settings[prop] = options[prop];
 			}
 		}
-    }
-	
+	}
+	if(settings.stopwordsRemove)
+	{
+		
+		settings.data = settings.data.removeStopWords();
+		
+	}
+	console.log( settings.data);
 	settings.data = wordFrequency(settings.data);
 	
 	self.init = function(options){
 		
 		linearFontScale = d3.scale.linear()
-					  .domain([1, 10])
-					  .range([settings.fontsize_range.min, settings.fontsize_range.max]);
-					  
+		.domain([1, 10])
+		.range([settings.fontsize_range.min, settings.fontsize_range.max]);
+		
 		svg = d3.select(settings.container)
-				.append("svg")
-				.attr("width", settings.width)
-				.attr("height", settings.height)
-				.style("background",settings.background)
-				.on("dblclick", function(){ self.redraw();
-			});
-			
-	var label = svg.selectAll(".place-label")
-				.data(settings.data)
-				.enter().append("text");
+		.append("svg")
+		.attr("width", settings.width)
+		.attr("height", settings.height)
+		.style("background",settings.background)
+		.on("dblclick", function(){ self.redraw();
+		});
+		
+		var label = svg.selectAll(".place-label")
+		.data(settings.data)
+		.enter().append("text");
 		
 		label.attr("class", "place-label")
-			.attr("font-family", function(d){ return (settings.font_families.length > 1) ? self.getRandom(settings.font_families): settings.font_families;})
-			.on("click", function(d){
-				console.log(d.word);
-			})
-			.on("mouseover", function(d){
-				defaultcolor = d.color;
-				d3.select(this).style("fill" , settings.hover_color);
-			})
-			.on("mouseout", function(d){
-					d3.select(this).style("fill" , defaultcolor);
-			});
+		.attr("font-family", function(d){ return (settings.font_families.length > 1) ? self.getRandom(settings.font_families): settings.font_families;})
+		.on("click", function(d){
+			console.log(d.word);
+		})
+		.on("mouseover", function(d){
+			defaultcolor = d.color;
+			d3.select(this).style("fill" , settings.hover_color);
+		})
+		.on("mouseout", function(d){
+			d3.select(this).style("fill" , defaultcolor);
+		});
 
 		label.style("font-size", function(d){
 			var font_size = linearFontScale(d.size);
@@ -198,11 +676,11 @@ var WordCrowd = function (options){
 		})
 		.style("fill", function(d){
 			d.color = settings.colors;
-            if(settings.colors == "random"){
-                 d.color = self.getRandomColor();
-            }
-            return d.color;
-         });
+			if(settings.colors == "random"){
+				d.color = self.getRandomColor();
+			}
+			return d.color;
+		});
 		
 		label.text(function(d) { return d.word; })
 		.each(function(d, i){
@@ -215,7 +693,7 @@ var WordCrowd = function (options){
 					break;
 				}
 				d3.select(this).attr("transform", function(d , i){
-						return d.word.length  > 10 ? "rotate(0)" : "rotate("+self.getRandom(settings.angles)+")";
+					return d.word.length  > 10 ? "rotate(0)" : "rotate("+self.getRandom(settings.angles)+")";
 				});
 				self.move(this);
 				tried++;
@@ -257,7 +735,7 @@ var WordCrowd = function (options){
 		var  yOff =  z.nextGaussian() * 50;
 		return [centerVertLine + xOff, centerHorizLine + yOff];
 	};
-			
+	
 	self.is_collide = function(that){
 
 		var DO_NOTHING = false;
